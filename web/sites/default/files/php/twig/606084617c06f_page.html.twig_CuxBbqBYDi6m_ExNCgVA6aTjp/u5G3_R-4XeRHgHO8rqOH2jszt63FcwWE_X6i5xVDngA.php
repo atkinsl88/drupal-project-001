540efb01794f4b041c8bf4d3b23eb17766d1b63c
atkinsl88 <liamatkins24@gmail.com>
2021-03-28 16:57:16 +0100

    commit update

---
+++ web/sites/default/files/php/twig/606084617c06f_page.html.twig_CuxBbqBYDi6m_ExNCgVA6aTjp/u5G3_R-4XeRHgHO8rqOH2jszt63FcwWE_X6i5xVDngA.php
@@ -0,0 +1,165 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/icecream/templates/page.html.twig */
+class __TwigTemplate_6476ae7a4b0f48499d513a37ccd608cef692d3df2dbd5c182d75962265f1e2f4 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 9];
+        $filters = ["escape" => 4];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<div class=\"layout-container\">
+
+  <header role=\"banner\">
+    ";
+        // line 4
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "header", [])), "html", null, true);
+        echo "
+  </header>
+
+  ";
+        // line 7
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "primary_menu", [])), "html", null, true);
+        echo "
+
+  ";
+        // line 9
+        if ($this->getAttribute(($context["page"] ?? null), "banner_top", [])) {
+            // line 10
+            echo "  <aside class=\"layout-banner-top\" role=\"banner\">
+    ";
+            // line 11
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "banner_top", [])), "html", null, true);
+            echo "
+  </aside>
+  ";
+        }
+        // line 14
+        echo "
+  ";
+        // line 15
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "help", [])), "html", null, true);
+        echo "
+
+  <main role=\"main\">
+    <a id=\"main-content\" tabindex=\"-1\"></a>";
+        // line 19
+        echo "
+    <div class=\"layout-content\">
+      ";
+        // line 21
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "content", [])), "html", null, true);
+        echo "
+
+      ";
+        // line 23
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "content_bottom", [])), "html", null, true);
+        echo "
+    </div>";
+        // line 25
+        echo "
+  </main>
+
+  ";
+        // line 28
+        if ($this->getAttribute(($context["page"] ?? null), "banner_bottom", [])) {
+            // line 29
+            echo "  <aside class=\"layout-banner-bottom\" role=\"banner\">
+    ";
+            // line 30
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "banner_bottom", [])), "html", null, true);
+            echo "
+  </aside>
+  ";
+        }
+        // line 33
+        echo "
+  ";
+        // line 34
+        if ($this->getAttribute(($context["page"] ?? null), "footer", [])) {
+            // line 35
+            echo "    <footer role=\"contentinfo\">
+      ";
+            // line 36
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "footer", [])), "html", null, true);
+            echo "
+    </footer>
+  ";
+        }
+        // line 39
+        echo "
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/icecream/templates/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  134 => 39,  128 => 36,  125 => 35,  123 => 34,  120 => 33,  114 => 30,  111 => 29,  109 => 28,  104 => 25,  100 => 23,  95 => 21,  91 => 19,  85 => 15,  82 => 14,  76 => 11,  73 => 10,  71 => 9,  66 => 7,  60 => 4,  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/icecream/templates/page.html.twig", "/var/www/html/web/themes/custom/icecream/templates/page.html.twig");
+    }
+}
